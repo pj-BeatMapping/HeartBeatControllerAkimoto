ff1b9878f434a4a543505d23ba9de34f7dd52af2
Wakua <aim1better2@gmail.com>
2016-09-20 23:40:27 +0900

    add beat generator

---
+++ src/BeatGenerator.cpp
@@ -0,0 +1,9 @@
+//
+//  BeatGenerator.cpp
+//  HeartBeatController
+//
+//  Created by 秋元和久 on 2016/09/20.
+//
+//
+
+#include <stdio.h>
---
+++ src/BeatGenerator.h
@@ -0,0 +1,13 @@
+//
+//  BeatGenerator.h
+//  HeartBeatController
+//
+//  Created by 秋元和久 on 2016/09/20.
+//
+//
+
+#ifndef BeatGenerator_h
+#define BeatGenerator_h
+
+
+#endif /* BeatGenerator_h */
